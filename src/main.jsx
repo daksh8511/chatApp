a78d7f8a6966043b5fb4a6dfdaf25ee7530e4e5b
mayur <daxsathwara102@gmail.com>
2025-05-14 16:42:38 +0530

    commited

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import AppContextProvider from "./Context/AppContext.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/chatApp">
     <AppContextProvider>
       <App />
     </AppContextProvider>
